5e078e5d8a8a1e70009f96bbb054e517c16b48f4
tilekbergen <tilekbergen.third@gmail.com>
2020-12-27 20:44:29 +0600

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const section = document.querySelector("section")
+function snow(){
+    let i = 1
+    while(i < 300){
+        let size = Math.random() * 10
+        let x = Math.floor(Math.random() * window.innerWidth)
+        let y = Math.floor(Math.random() * window.innerHeight)
+        const snow = document.createElement("i")
+        snow.style.top = y + "px"
+        snow.style.left = x + "px"
+        snow.style.height = 1 + size + "px"
+        snow.style.width = 1 + size + "px"
+        section.appendChild(snow)
+        i++;
+    }
+}
+snow()
